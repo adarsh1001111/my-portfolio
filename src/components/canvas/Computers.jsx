4d218b958347b688a3b7c9a016480a1bc5d163e7
Adarsh Amit <adarsh.amit1001@gmail.com>
2024-03-17 01:27:46 +0530

    pos of mobile enhanced

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -26,7 +26,7 @@ const Computers = ({ isMobile }) => {
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.4 : 0.75}
-        position={isMobile ? [0, -1.5, -1.2] : [0, -2.9, -1.5]}
+        position={isMobile ? [0, -1, -1.2] : [0, -2.9, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
