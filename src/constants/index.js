f2113fddad2db691df50e29d5252f58eaec143ff
Adarsh Amit <adarsh.amit1001@gmail.com>
2024-03-16 22:01:01 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,282 @@
+import {
+  dsa,
+  hack,
+  github1,
+  linux,
+  clang,
+  canva,
+  sql,
+  cpp,
+  ds,
+  github,
+  sih,
+  trophy,
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  rnm,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  python,
+} from "../assets";
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+const services = [
+  {
+    title: "Competitive Programmer",
+    icon: trophy,
+  },
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Python Developer",
+    icon: python,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+];
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "GitHub",
+    icon: github,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "C++",
+    icon: cpp,
+  },
+
+  {
+    name: "SQL",
+    icon: sql,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+
+  {
+    name: "C-language",
+    icon: clang,
+  },
+  {
+    name: "python",
+    icon: python,
+  },
+  {
+    name: "canva",
+    icon: canva,
+  },
+];
+const experiences = [
+  {
+    title: "1st Year",
+    company_name: "JIIT",
+    icon: dsa,
+    iconBg: "#383E56",
+    date: "Nov-2022",
+    points: [
+      "🎓 Admitted to Jaypee Institute of Information Technology",
+      "📚 Started first year of studies",
+      "🐍 Learned Python (basic) along with C language and Data Structures and Algorithms (DSA) skills.",
+    ],
+  },
+  {
+    title: "2nd Year",
+    company_name: "JIIT",
+    icon: hack,
+    iconBg: "#E6DEDD",
+    date: "August-2023",
+    points: [
+      "📖 Entered second year at Jaypee Institute of Information Technology",
+      "💻 Began engaging in competitive programming",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "JIIT",
+    icon: html,
+    iconBg: "#383E56",
+    date: "September-2023",
+    points: [
+      "💻 Engaged in web development, learning various technology stacks including HTML, CSS, JavaScript along with SQL database",
+      "🚀 Initiated advanced Python programming skills development",
+      ,
+    ],
+  },
+  {
+    title: "Linux",
+    company_name: "Git+Linux scripting",
+    icon: linux,
+    iconBg: "#E6DEDD",
+    date: "October-November-2023",
+    points: [
+      "💡 Explored Bash scripting and Linux commands.",
+      "🛠️ Gained proficiency in Git and GitHub version control",
+    ],
+  },
+  {
+    title: "Data-Science",
+    company_name: "Bash-Scripting",
+    icon: ds,
+    iconBg: "#E6DEDD",
+    date: "December-2023",
+    points: [
+      "This journey involved learning essential concepts like data preprocessing, statistical analysis, and machine learning algorithms, setting the stage for understanding the transformative potential of data-driven decision-making in various domains.",
+    ],
+  },
+  {
+    title: "Competitive-Programming",
+    company_name: "LeetCode,HackerRank,CodeChef",
+    icon: trophy,
+    iconBg: "#E6DEDD",
+    date: "January-February-2024",
+    points: [
+      "🌟 Started practicing on LeetCode, CodeChef, and HackerRank",
+      "🏋️‍♂️ Maintained a continuous streak on all platforms",
+      "💻 Engaged in development using React,Node.js, Three.js, Tailwind CSS, and other related technologies.",
+    ],
+  },
+  {
+    title: "Currently",
+    company_name: "CP+Development",
+    icon: reactjs,
+    iconBg: "#E6DEDD",
+    date: "Present-Date",
+    points: [
+      "🏆 Continued exploration of web development",
+      "🌟 Achieved a 2-star rating on CodeChef",
+      "🛠️ Practiced building projects using the acquired web development skills.",
+      "🔍 Further honed skills in web development",
+      "🚀 Worked on advanced projects to solidify understanding and expertise in web technologies.",
+    ],
+  },
+];
+const testimonials = [
+  {
+    testimonial:
+      "I had the pleasure of working alongside Adarsh Amit on the 'Game Rocket and Meteors' project,his collaborative nature and commitment to excellence makes him a valuable asset to any team, and I wholeheartedly recommend him for future projects requiring top-notch C++ development skills.",
+    name: "Nirajan Kumar Verma",
+    designation: "B.Tech in CSE ",
+    company: " JIIT",
+    image:
+      "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7",
+  },
+  {
+    testimonial:
+      "Working with Adarsh on my project has been an absolute pleasure. His expertise and dedication significantly contributed to the success of our endeavor. Adarsh's commitment to excellence, prompt responses, and innovative problem-solving skills truly set him apart.",
+    name: "Abhijeet Kumar",
+    designation: "B.Tech+M.Tech in CSE ",
+    company: " JIIT",
+    image:
+      "https://media.licdn.com/dms/image/D5603AQHqYrmtEx3ulg/profile-displayphoto-shrink_800_800/0/1710067669030?e=1715817600&v=beta&t=HMG8kmw6B-Mo4EkuGe48dUglAml-NUem0EuyWm2kqvs",
+  },
+  {
+    testimonial:
+      "Adarsh played a pivotal role in our project's success. His expertise, dedication, and collaborative approach were truly commendable. I confidently endorse Adarsh for his outstanding contributions and teamwork.",
+    name: "Viyom Shukla",
+    designation: "B.Tech+M.Tech in CSE ",
+    company: " JIIT",
+    image:
+      "https://media.licdn.com/dms/image/D5603AQEdp5DInhX7TQ/profile-displayphoto-shrink_800_800/0/1704691923739?e=1715817600&v=beta&t=N1hckSkouGzfEe1luiXnWWwQU-wdw-4NBNCyJWyYwOY",
+  },
+];
+const projects = [
+  {
+    name: "Rocket-And-Meteros",
+    description:
+      "This is a personal Project created using c++ using ncurses library for MAC system.Check it out on GitHub!!.",
+    tags: [
+      {
+        name: "C++",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "OOPS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "ncurses",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: rnm,
+    source_code_link: "https://github.com/adarsh1001111/RocketAndMeteors-MAC-",
+  },
+  {
+    name: "SIH",
+    description:
+      "The project targets to Tourism Domain Of the Govt. sector,integration of all the govt. affiliates databases is the core concept of our project!.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: sih,
+    source_code_link: "https://github.com/adarsh1001111/SIH",
+  },
+];
+export { services, technologies, experiences, testimonials, projects };
